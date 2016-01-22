0e58373fa2ffb0857135865590190d026bd0a402
Tyler Gilmore <tgilmore@mailctp.com>
2016-01-22 16:44:27 -0600

    Initial commit.

---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Calculator.Add(1, 1));
+
+            //Console.WriteLine(Calculator.Subtract(1, 1));
+
+            //Console.WriteLine(Calculator.Multiply(1, 1));
+
+            //Console.WriteLine(Calculator.Divide(1, 1));
+
+            //Console.ReadLine();
+        }
+    }
+}
